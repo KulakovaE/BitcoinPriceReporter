fa48dde1750637ea584ff00ba1d9411df8ff38e7
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-19 10:44:21 +0100

    Working on the UIView

---
+++ BitcoinPriceReporter/Model/CoinManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoinManager.swift
+//  BitcoinPriceReporter
+//
+//  Created by Darko Kulakov on 2019-11-19.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+}
